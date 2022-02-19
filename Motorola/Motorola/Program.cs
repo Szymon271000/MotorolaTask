1c19ab1e09ea132ef0d50be9b4f79006d2843c7b
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-02-19 20:13:41 +0100

    MotorolaTask

---
+++ Motorola/Motorola/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace Motorola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\Users\huber\OneDrive\Desktop\MotorolaTask\Motorola\Motorola\Words.txt";
+            string[] WordsFromFile = File.ReadAllText(FilePath).Split("\r\n");
+            Random Rnd = new Random();
+
+            List<string> WordsToGame = new List<string>();
+            for (int i = 0; i < 4;)
+            {
+                int Index = Rnd.Next(0, WordsFromFile.Length);
+                string WordsToMemory = WordsFromFile[Index];
+                if (!WordsToGame.Contains(WordsToMemory))
+                {
+                    WordsToGame.Add(WordsToMemory);
+                    WordsToGame.Add(WordsToMemory);
+                    i++;
+                }
+
+            }
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                int Index = Rnd.Next(0, WordsToGame.Count);
+                int SecondIndex = Rnd.Next(0, WordsToGame.Count);
+                string Tmp = WordsToGame[Index];
+                WordsToGame[Index] = WordsToGame[SecondIndex];
+                WordsToGame[SecondIndex] = Tmp;
+            }
+
+            
+            var Board = WordsToGame.Select(x => new Word { Text = x }).ToList();
+
+            Console.Write("  ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            
+            for (int i = 0; i < Board.Count; i++)
+            {
+                
+                if (i != 0 && i % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+                if (i % 4 == 0)
+                {
+                    Console.Write((char)('A' + i ) + " ");
+                }
+                Console.Write(Board[i] + " ");
+            }
+            
+
+         }
+    }
+}
---
+++ Motorola/Motorola/Word.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motorola
+{
+    class Word
+    {
+        public string Text { get; set; }
+
+        public bool Shown { get; set; }
+
+        public bool Paired { get; set; }
+
+        public override string ToString()
+        {
+            if (Shown || Paired)
+            {
+                return Text;
+            }
+            else
+                return "X";
+        }
+    }
+}
